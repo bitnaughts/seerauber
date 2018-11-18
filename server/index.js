a6fba955e7951e5a3f40f8f3fb65ac73fa6b6597
Adrian Darian <adarian@ucmerced.edu>
2018-11-17 23:48:04 -0800

    revert back to port 3000

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/../build')));
 console.log(path.join(__dirname, '/../build'));
 
-const port = process.env.PORT || 7070;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`));
 
