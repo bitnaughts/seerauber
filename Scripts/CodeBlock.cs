0795bf7065ca4a2b033a954b57bfa3ed63650854
Brian <brianhungerman@gmail.com>
2018-11-17 21:27:46 -0800

    9:27

--- Scripts/CodeBlock.cs
+++ Scripts/CodeBlock.cs
@@ -1,75 +1,83 @@
 using System;
 public class CodeBlock {
-	//BRIAN'S UI CODE
-	public bool isDragging = false;
-	public bool isHovering = false;
+    //BRIAN'S UI CODE
+    public bool isDragging = false;
+    public bool isHovering = false;
 
-	public int positionX = 0; // for draggy drags
-	public int positionY = 0;
+    public int positionX = 0; // for draggy drags
+    public int positionY = 0;
 
-	public int sizeX = 200; //scales with parameters...
-	public int sizeY = 100; //set size?
-	public CodeBlock[] nestedBlocks;
-	public string command;
-	public string parameter;
+    public int sizeX = 200; //scales with parameters...
+    public int sizeY = 100; //set size?
+    public CodeBlock[] nestedBlocks;
+    public string command;
+    public string parameter;
 
-	//const string[] commandlist = { "loop", "task", "check","variable","math" };
-	//const string[] tasklist = { "sleep", "loadCannon", "eat", "drink" };
-	//if command = loop, then parameter is "X-Y"
-	//if command = task, then parameter is from taskList
-	//if command = check, parameter is boolean statement
+    //const string[] commandlist = { "loop", "task", "check","variable","math" };
+    //const string[] tasklist = { "sleep", "loadCannon", "eat", "drink" };
+    //if command = loop, then parameter is "X-Y"
+    //if command = task, then parameter is from taskList
+    //if command = check, parameter is boolean statement
 
-	public CodeBlock () {
+    public CodeBlock () {
         //Randomizer.getInteger()
-	    nestedBlocks = new CodeBlock[4];
-		nestedBlocks[0] = new CodeBlock(new CodeBlock[1] {new CodeBlock("task","loadCannon")},"check","isCombat");
-		nestedBlocks[1] = new CodeBlock(new CodeBlock[1] {new CodeBlock(new CodeBlock[1]{new CodeBlock("task","eat"),"check","getHunger()~<~10")},"check","isDay");
-		nestedBlocks[2] = new CodeBlock(new CodeBlock[1] {new CodeBlock("task","sail")},"check","isSailing");
-		nestedBlocks[3] = new CodeBlock(new CodeBlock[1] {new CodeBlock("task","sleep")},"check","isNight");
-	}
-	public CodeBlock (CodeBlock[] blockarray, string com, string param) {
-		nestedBlocks = blockarray;
-		command = com;
-		parameter = param;
-	}
-	public CodeBlock (string com, string param) {
-		command = com;
-		parameter = param;
-	}
-
-	public void addToEnd (string command, string parameter) {
-		if (this.nestedBlocks == null) {
-			CodeBlock newBlock = new CodeBlock (command, parameter);
-			CodeBlock[] newBlockArray = { newBlock };
-			this.nestedBlocks = newBlockArray;
-		} else {
-			CodeBlock[] newBlockArray = new CodeBlock[this.nestedBlocks.Length + 1];
-			CodeBlock newBlock = new CodeBlock (command, parameter);
-			for (int i = 0; i != newBlockArray.Length; i++) {
-				newBlockArray[i] = this.nestedBlocks[i];
-			}
-			newBlockArray[newBlockArray.Length - 1] = newBlock;
-			this.nestedBlocks = newBlockArray;
-		}
-        
-         
-	}
-	public void addToIndex (string command, string parameter, int index) {
-		CodeBlock[] newBlockArray = new CodeBlock[this.nestedBlocks.Length + 1];
-		CodeBlock newBlock = new CodeBlock (command, parameter);
-		for (int i = 0; i != newBlockArray.Length; i++) {
-			if (i == index) {
-				newBlockArray[i] = newBlock;
-			} else {
-				newBlockArray[i] = this.nestedBlocks[i];
-			}
-		}
-		this.nestedBlocks = newBlockArray;
-	}
-	public void move (int initial,int final)
+        nestedBlocks = new CodeBlock[4];
+        nestedBlocks[0] = new CodeBlock (new CodeBlock[1] { new CodeBlock ("task", "loadCannon") }, "check", "isCombat");
+        nestedBlocks[1] = new CodeBlock (new CodeBlock[1] { new CodeBlock ("check", "hunger~<~10") }, "check", "isDay");
+		nestedBlocks[1].nestedBlocks = new CodeBlock[1]{ new CodeBlock("task","eat")};
+        nestedBlocks[2] = new CodeBlock (new CodeBlock[1] { new CodeBlock ("task", "sail") }, "check", "isSailing");
+        nestedBlocks[3] = new CodeBlock (new CodeBlock[1] { new CodeBlock ("task", "sleep") }, "check", "isNight");
+    }
+    public CodeBlock (CodeBlock[] blockarray, string com, string param) {
+        nestedBlocks = blockarray;
+        command = com;
+        parameter = param;
+    }
+    public CodeBlock (string com, string param) {
+        command = com;
+        parameter = param;
+    }
+	public string printOut(CodeBlock[] blockArr)
 	{
-		CodeBlock temp = this.nestedBlocks[initial];
-		this.nestedBlocks[initial]=this.nestedBlocks[final];
-		this.nestedBlocks[final]=temp;
+		string out;
+		foreach (CodeBlock block in blocks)
+		{
+            //return a string to print to console
+			out += (CodeBlock.parameter); //isn't a function lol
+			if
+		}
 	}
+    public void addToEnd (string command, string parameter) {
+        if (this.nestedBlocks == null) {
+            CodeBlock newBlock = new CodeBlock (command, parameter);
+            CodeBlock[] newBlockArray = { newBlock };
+            this.nestedBlocks = newBlockArray;
+        } else {
+            CodeBlock[] newBlockArray = new CodeBlock[this.nestedBlocks.Length + 1];
+            CodeBlock newBlock = new CodeBlock (command, parameter);
+            for (int i = 0; i != newBlockArray.Length; i++) {
+                newBlockArray[i] = this.nestedBlocks[i];
+            }
+            newBlockArray[newBlockArray.Length - 1] = newBlock;
+            this.nestedBlocks = newBlockArray;
+        }
+
+    }
+    public void addToIndex (string command, string parameter, int index) {
+        CodeBlock[] newBlockArray = new CodeBlock[this.nestedBlocks.Length + 1];
+        CodeBlock newBlock = new CodeBlock (command, parameter);
+        for (int i = 0; i != newBlockArray.Length; i++) {
+            if (i == index) {
+                newBlockArray[i] = newBlock;
+            } else {
+                newBlockArray[i] = this.nestedBlocks[i];
+            }
+        }
+        this.nestedBlocks = newBlockArray;
+    }
+    public void move (int initial, int final) {
+        CodeBlock temp = this.nestedBlocks[initial];
+        this.nestedBlocks[initial] = this.nestedBlocks[final];
+        this.nestedBlocks[final] = temp;
+    }
 }
--- Scripts/CodeBlockController.cs
+++ Scripts/CodeBlockController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodeBlockController : MonoBehaviour {
 
@@ -24,55 +25,65 @@ public class CodeBlockController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		print ("ref:" + reference);
-		if (true) {//reference != null) {
-
-			height = (reference.nestedBlocks == null) ? 0 : reference.nestedBlocks.Length; //  recursivelyGetHeight(reference);//l.nestedBlocks.Length;
-			transform.parent.GetChild (1).GetChild(13).gameObject.GetComponent<Text> ().text = reference.parameter;
-				
-
-		//random amount of subchildren
-		this.GetComponent<RectTransform> ().sizeDelta = new Vector2 (200, 20);
-		transform.parent.GetChild (1).GetComponent<RectTransform> ().sizeDelta = new Vector2 (200, height * 25 + 20);
-		//DRAG START
-		if (Input.GetMouseButtonDown (0)) {
-			mouseLocation = Input.mousePosition;
-			isHovering = false;
-
-			float xMin = this.GetComponent<RectTransform> ().position.x;
-			float xMax = this.GetComponent<RectTransform> ().position.x + 200f;//this.GetComponent<RectTransform> ().sizeDelta.x; // graphs[graph].positionX + graphs[graph].width;
-			float yMin = this.GetComponent<RectTransform> ().position.y; //graphs[graph].positionY;
-			float yMax = this.GetComponent<RectTransform> ().position.y + 30f;//this.GetComponent<RectTransform> ().sizeDelta.y;
-
-			RectTransform blockTransform = this.GetComponent<RectTransform> ();
-
-			if (clickOn (-blockTransform.sizeDelta.x / 2, blockTransform.sizeDelta.x / 2, -blockTransform.sizeDelta.y / 2, blockTransform.sizeDelta.y / 2, mouseLocation, this.GetComponent<RectTransform> ().position)) {
-			
-				isHovering = true;
+		if (true) { //reference != null) {
+			height = 0;
+			if (reference.nestedBlocks != null) {
+				for (int i = 0; i < reference.nestedBlocks.Length; i++) {
+					if (reference.nestedBlocks[i].nestedBlocks != null) {
+						for (int j = 0; j < reference.nestedBlocks[i].nestedBlocks.Length; j++) {
+							height++;
+						}
+					}
+					height++;
+				}
 
 			}
 
-		}
-		//DRAG MOVE
-		if (Input.GetMouseButton (0)) {
-			
-	
-			newMouseLocation = Input.mousePosition;
+			//height = (reference.nestedBlocks == null) ? 0 : // recursivelyGetHeight (reference); //l.nestedBlocks.Length;
+			transform.parent.GetChild (1).GetChild (0).GetChild (13).gameObject.GetComponent<Text> ().text = reference.parameter;
 
-			if (isHovering) {
-						print ("yes, dragging...");
-				Vector2 newPosition = this.GetComponent<RectTransform> ().position;
-				newPosition.x += -(mouseLocation.x - newMouseLocation.x);
-				newPosition.y += -(mouseLocation.y - newMouseLocation.y);
-				this.GetComponent<RectTransform> ().position = newPosition;
+			//random amount of subchildren
+			this.GetComponent<RectTransform> ().sizeDelta = new Vector2 (200, 20);
+			transform.parent.GetChild (1).GetComponent<RectTransform> ().sizeDelta = new Vector2 (200, height * 25 + 20);
+			//DRAG START
+			if (Input.GetMouseButtonDown (0)) {
+				mouseLocation = Input.mousePosition;
+				isHovering = false;
+
+				float xMin = this.GetComponent<RectTransform> ().position.x;
+				float xMax = this.GetComponent<RectTransform> ().position.x + 200f; //this.GetComponent<RectTransform> ().sizeDelta.x; // graphs[graph].positionX + graphs[graph].width;
+				float yMin = this.GetComponent<RectTransform> ().position.y; //graphs[graph].positionY;
+				float yMax = this.GetComponent<RectTransform> ().position.y + 30f; //this.GetComponent<RectTransform> ().sizeDelta.y;
+
+				RectTransform blockTransform = this.GetComponent<RectTransform> ();
+
+				if (clickOn (-blockTransform.sizeDelta.x / 2, blockTransform.sizeDelta.x / 2, -blockTransform.sizeDelta.y / 2, blockTransform.sizeDelta.y / 2, mouseLocation, this.GetComponent<RectTransform> ().position)) {
+
+					isHovering = true;
+
+				}
+
 			}
-			mouseLocation = newMouseLocation;
-		}
+			//DRAG MOVE
+			if (Input.GetMouseButton (0)) {
 
-		Vector2 snapper = this.transform.localPosition;
-		int step = 30;
-		snapper = new Vector2 ((((int) snapper.x + step / 2) / step) * step, (((int) snapper.y + step / 2) / step) * step);
-		transform.parent.GetChild (1).localPosition = snapper;
-		//this.transform.localPosition 
+				newMouseLocation = Input.mousePosition;
+
+				if (isHovering) {
+					print ("yes, dragging...");
+					Vector2 newPosition = this.GetComponent<RectTransform> ().position;
+					newPosition.x += -(mouseLocation.x - newMouseLocation.x);
+					newPosition.y += -(mouseLocation.y - newMouseLocation.y);
+					this.GetComponent<RectTransform> ().position = newPosition;
+				}
+				mouseLocation = newMouseLocation;
+			}
+
+			Vector2 snapper = this.transform.localPosition;
+			int step = 30;
+			snapper = new Vector2 ((((int) snapper.x + step / 2) / step) * step, (((int) snapper.y + step / 2) / step) * step);
+			transform.parent.GetChild (1).localPosition = snapper;
+			//this.transform.localPosition 
 		} else height = 0;
 	}
 	public bool clickOn (float xMin, float xMax, float yMin, float yMax, Vector2 mousePosition, Vector2 otherPosition) {
@@ -82,8 +93,10 @@ public class CodeBlockController : MonoBehaviour {
 	}
 
 	public int recursivelyGetHeight (CodeBlock refer) {
-		for (int i = 0; i < refer.nestedBlocks.Length; i++) {
-			return recursivelyGetHeight (refer.nestedBlocks[i]) + 1;
+		if (refer.nestedBlocks != null) {
+			for (int i = 0; i < refer.nestedBlocks.Length; i++) {
+				return recursivelyGetHeight (refer.nestedBlocks[i]) + 1;
+			}
 		}
 		return 1;
 	}
